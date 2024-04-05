28eb4932c31c33769a9948e74f4fedb95c5bef7a
Lerian Febriana, A.Md.Kom <kanglerian@gmail.com>
2024-04-05 08:55:18 +0700

    page register

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -6,6 +6,14 @@ import { checkTokenExpiration, forbiddenAccess } from '../../middlewares/middlew
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  const [nameReg, setNameReg] = useState('');
+  const [emailReg, setEmailReg] = useState('');
+  const [schoolReg, setSchoolReg] = useState('');
+  const [phoneReg, setPhoneReg] = useState('');
+  const [passwordReg, setPasswordReg] = useState('');
+  const [passwordConfReg, setPasswordConfReg] = useState('');
+
   const navigate = useNavigate();
 
   const loginFunc = async (e) => {
@@ -20,11 +28,29 @@ const Login = () => {
         navigate('/dashboard')
       })
       .catch((error) => {
-        if(error.response.status == 401){
+        if (error.response.status == 401) {
           return alert(error.response.data.message);
         } else {
           console.log(error);
         }
+      });
+  }
+
+  const registerFunc = async (e) => {
+    e.preventDefault();
+    await axios.post(`${process.env.BACKEND_SERVER}/api/auth/psikotest/register`, {
+      name: nameReg,
+      school: schoolReg,
+      email: emailReg,
+      phone: phoneReg,
+      password: passwordReg,
+      password_confirmation: passwordConfReg
+    })
+      .then((response) => {
+        alert(response.data.message)
+      })
+      .catch((error) => {
+        console.log(error);
       });
   }
 
@@ -35,10 +61,22 @@ const Login = () => {
 
   return (
     <section>
+      <h2>Login Disini</h2>
       <form onSubmit={loginFunc}>
         <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder='Email' />
         <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Password' />
         <button type='submit'>Login</button>
+      </form>
+      <hr />
+      <h2>Daftar Disini</h2>
+      <form onSubmit={registerFunc}>
+        <input type="text" value={nameReg} onChange={(e) => setNameReg(e.target.value)} placeholder='Name' />
+        <input type="text" value={schoolReg} onChange={(e) => setSchoolReg(e.target.value)} placeholder='school' />
+        <input type="email" value={emailReg} onChange={(e) => setEmailReg(e.target.value)} placeholder='email' />
+        <input type="number" value={phoneReg} onChange={(e) => setPhoneReg(e.target.value)} placeholder='phone' />
+        <input type="password" value={passwordReg} onChange={(e) => setPasswordReg(e.target.value)} placeholder='Password' />
+        <input type="password" value={passwordConfReg} onChange={(e) => setPasswordConfReg(e.target.value)} placeholder='Konfirmasi Password' />
+        <button type='submit'>Daftar</button>
       </form>
     </section>
   )
